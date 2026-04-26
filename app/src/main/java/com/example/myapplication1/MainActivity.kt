e9122fb7f08220b9c171316c5e71165cbcdddb21
Prof. André Luiz Braga, D.Sc <andre.luiz.braga2000@gmail.com>
2026-04-26 11:27:09 -0300

    Versao COMPOSE

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication1.ui.theme.MyApplication1Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,22 +31,47 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplication1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    val contadorGlobal = remember { mutableStateOf(0) }
+                    Column(modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally) {
+                        Greeting(
+                            name = "Android Real"
+                        )
+                        ContarButton(contadorGlobal)
+                        InputNumero(contadorGlobal)
+                    }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun ContarButton(contador: MutableState<Int>) {
+    Button(onClick = {
+        contador.value++
+    }) {
+        Text(text = "contar ${contador.value}")
+    }
+}
+@Composable
+fun InputNumero(contador: MutableState<Int>) {
+    var texto by remember { mutableStateOf("") }
+    TextField(
+        value = texto,
+        onValueChange = { novoTexto ->
+            texto = novoTexto
+            novoTexto.toIntOrNull()?.let { novoValor ->
+                contador.value = novoValor
+            }
+        },
+        label = { Text("Definir contador") },
+        modifier = Modifier.padding(16.dp)
+    )
+}
+@Composable
+fun Greeting(name: String) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello $name!"
     )
 }
 
@@ -42,6 +79,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyApplication1Theme {
-        Greeting("Android")
+        val contadorPreview = remember { mutableStateOf(0) }
+        Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Greeting("AndroidPreview")
+            ContarButton(contadorPreview)
+            InputNumero(contadorPreview)
+        }
     }
 }
+
+
